740c7d3ade234785c9a28bdf48b9906bef7e6875
Maitreyee Joshi <maitreyeejoshi@maitreyees-macbook-pro.local>
2018-09-13 05:44:58 -0700

    Initial Commit

---
+++ Firewood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firewood
+//
+//  Created by Maitreyee Joshi on 9/13/18.
+//  Copyright © 2018 Maitreyee Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
